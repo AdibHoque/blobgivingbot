472ba7264ed4bebe4e6a6edaf375ae0637a06320
AdibHoque <46087857+AdibHoque@users.noreply.github.com>
2019-03-26 18:08:24 +0600

    Update example_config.py

--- example_config.py
+++ example_config.py
@@ -2,15 +2,15 @@
 
 import datetime
 
-token = 'beep.boop.secret'
+token = 'NTAwMjM5OTEzMzUwOTg3Nzk3.D3unTA.-5OCzewu_qeGfHuIHIx2GIqNxBA'
 
-prefix = '$'
+prefix = 'e!'
 owner_id = 69198249432449024
 
 giveaway_channel = 384121527437885440
 giveaway_emoji = 384394753749417996
 
-giveaway_duration = datetime.timedelta(days=1)
+giveaway_duration = datetime.timedelta(days=2)
 
 # channels in which the bot responds to commands, all others are ignored
 command_channels = {384121527437885440}
