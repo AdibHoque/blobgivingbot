8f0ea3505d1df147c14c80f15f6536bc57cfbcd8
AdibHoque <46087857+AdibHoque@users.noreply.github.com>
2019-03-26 18:11:57 +0600

    Update example_config.py

--- example_config.py
+++ example_config.py
@@ -5,12 +5,12 @@ import datetime
 token = 'NTAwMjM5OTEzMzUwOTg3Nzk3.D3unTA.-5OCzewu_qeGfHuIHIx2GIqNxBA'
 
 prefix = 'e!'
-owner_id = 69198249432449024
+owner_id = 488353416599306270
 
-giveaway_channel = 384121527437885440
-giveaway_emoji = 384394753749417996
+giveaway_channel = 560072790880026654
+giveaway_emoji = 527116698566787072
 
 giveaway_duration = datetime.timedelta(days=2)
 
 # channels in which the bot responds to commands, all others are ignored
-command_channels = {384121527437885440}
+command_channels = {560072790880026654}
